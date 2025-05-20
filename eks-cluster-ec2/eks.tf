f973a59da2b7e5afa1c6249fe883eeaf033526e3
Dappyplay4u <jegede.oladapo@ymail.com>
2025-05-20 10:46:28 -0400

    updated port

--- eks-cluster-ec2/eks.tf
+++ eks-cluster-ec2/eks.tf
@@ -18,15 +18,7 @@ resource "aws_security_group" "online-shop-eks-cluster" {
 
     }
   }
-# Allow NodePort range (30000-32767)
-  ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow NodePort range for Kubernetes services"
-  }
-
+  
   #outbound traffic
   egress {
     from_port   = 0
